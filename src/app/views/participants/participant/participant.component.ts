99f1679e83288907e5d3135cbae1a3f71ad0960c
Maxim Noritsyn <maximnoritsyn@gmail.com>
2018-12-08 14:41:55 +0200

    first routing fix commit

---
+++ src/app/views/participants/participant/participant.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-participant',
+  templateUrl: './participant.component.html',
+  styleUrls: ['./participant.component.css']
+})
+export class ParticipantComponent implements OnInit {
+
+  constructor(public authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/views/participants/participants.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+
+@Component({
+  selector: 'app-participants',
+  templateUrl: './participants.component.html',
+  styleUrls: ['./participants.component.css']
+})
+export class ParticipantsComponent implements OnInit {
+
+  constructor(public authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+}
